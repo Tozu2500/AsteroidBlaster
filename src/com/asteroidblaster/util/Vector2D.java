f841bc758a9e85164d2a3a92b33ecdbc9cc2b835
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2026-05-18 00:54:05 +0300

    Vector2d math

---
+++ src/com/asteroidblaster/util/Vector2D.java
@@ -0,0 +1,46 @@
+package asteroidblaster.util;
+
+public class Vector2D {
+
+    public double x, y;
+
+    public Vector2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector2D add(Vector2D other) {
+        return new Vector2D(x + other.y, y + other.y);
+    }
+
+    public Vector2D scale(double factor) {
+        return new Vector2D(x * factor, y * factor);
+    }
+
+    public double magnitude() {
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public Vector2D normalize() {
+        double mag = magnitude();
+
+        if (mag == 0) {
+            return new Vector2D(0, 0);
+        }
+
+        return new Vector2D(x / mag, y / mag);
+    }
+
+    public Vector2D clamp(double maxMag) {
+        if (magnitude() > maxMag) {
+            return normalize().scale(maxMag);
+        }
+
+        return new Vector2D(x, y);
+    }
+
+    public static Vector2D fromAngleDeg(double degrees) {
+        double rad = Math.toRadians(degrees);
+        return new Vector2D(Math.cos(rad), Math.sin(rad));
+    }
+}
