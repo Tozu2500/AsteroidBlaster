09142cd8fcf11b4dc0ad61ced0b695da8d511034
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2026-05-26 00:23:30 +0300

    Starfield class start

---
+++ src/com/asteroidblaster/model/Starfield.java
@@ -0,0 +1,16 @@
+package com.asteroidblaster.model;
+
+import java.util.Random;
+
+import com.asteroidblaster.util.Constants;
+
+public class Starfield {
+
+    private final int[] starX, starY, brightness;
+    private final float[] size;
+    private static final Random random = new Random();
+
+    public Starfield(int screenWidth, int screenHeight) {
+        int starCount = Constants.STAR_COUNT;
+    }
+}
