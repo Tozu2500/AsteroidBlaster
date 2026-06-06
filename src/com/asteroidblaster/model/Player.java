d9e514eadd4b3758ceb4a8e3d70639afe42a816a
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2026-06-06 23:12:43 +0300

    Player constructor

--- src/com/asteroidblaster/model/Player.java
+++ src/com/asteroidblaster/model/Player.java
@@ -1,5 +1,27 @@
 package com.asteroidblaster.model;
 
+import com.asteroidblaster.util.Constants;
+
 public class Player {
+
+    public double x, y;
+    public double velocityX, velocityY;
+    public double angle;  // in degrees
+    public int lives;
+    public boolean thrusting;
+    public boolean dead;
+
+    private long invincibleUntil;
+    private int shootCooldown;
+
+    // Exhaust flicker effect
+    private int flameTick;
+
+    public Player(double x, double y) {
+        this.x = x;
+        this.y = y;
+        this.angle = 90;
+        this.lives = Constants.PLAYER_LIVES;
+    }
 
 }
